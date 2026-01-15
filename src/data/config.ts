6f8b00e07b777175ac6b84189cd15113dc8d2776
SWAGGA TH4 K1NG <97411896+SWAGGATH4K1NG@users.noreply.github.com>
2026-01-15 22:19:25 +0000

    Update title and add icon

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  title: "SWAGGA | Full-Stack Developer",
+  title: "SWAGGA — Creative Developer & Portfolio",
   description: {
     long: "Explore the portfolio of SWAGGA, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
